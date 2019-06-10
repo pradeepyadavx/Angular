a9f6f51de387a8b5e66e416bb7ad0437baa07a31
pradeep.yadav <pradeep.yadav@bdb.ai>
2019-06-10 14:52:24 +0530

    add types of component can be used as element as attr and class as wll

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // can be used as deirect elemnt
+ // selector: 'app-servers',
+  // can be used as attribute
+    // selector: '[app-servers]',
+
+    // can be used as class 
+    selector: '.app-servers',
+
+    // using html file path
+   // templateUrl:'./servers.component.html',
+
+  // inline teamplte 
   template: `<app-server></app-server>
                 <hr>
                 <app-server></app-server>`,
