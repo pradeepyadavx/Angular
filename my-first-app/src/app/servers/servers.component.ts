d28c8501d3ec8514726f4fccc2a58f70afd1a9a5
pradeep.yadav <pradeep.yadav@bdb.ai>
2019-06-10 15:54:18 +0530

    Databinding using property binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -13,14 +13,20 @@ import { Component, OnInit } from '@angular/core';
    // templateUrl:'./servers.component.html',
 
   // inline teamplte 
-  template: `<app-server></app-server>
-                <hr>
-                <app-server></app-server>`,
+  // template: `<app-server></app-server>
+  //               <hr>
+  //               <app-server></app-server>`,
+  templateUrl:'./servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer=false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer=true;
+    }, 2000);
+   }
 
   ngOnInit() {
   }
