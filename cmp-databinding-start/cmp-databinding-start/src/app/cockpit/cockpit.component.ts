7cc84dafacfca9bae8b93bb7eb704db66b949cab
pradeep.yadav <pradeep.yadav@bdb.ai>
2019-06-21 16:46:41 +0530

    local refrence in template

--- cmp-databinding-start/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -9,7 +9,7 @@ export class CockpitComponent implements OnInit {
   @ Output() serverCreated=new EventEmitter<{serverName:string,serverContent:string}>();
   @ Output('bpCreated') blueprintCreated=new EventEmitter<{serverName:string,serverContent:string}>();
 
-  newServerName = '';
+  //newServerName = '';
   newServerContent = '';
 
   constructor() { }
@@ -17,12 +17,13 @@ export class CockpitComponent implements OnInit {
   ngOnInit() {
   }
 
-  onAddServer() {
-    this.serverCreated.emit({serverName:this.newServerName,serverContent:this.newServerContent})
+  onAddServer(serverNameInput:HTMLInputElement) {
+      
+    this.serverCreated.emit({serverName:serverNameInput.value,serverContent:this.newServerContent})
   }
 
-  onAddBlueprint() {
-    this.blueprintCreated.emit({serverName:this.newServerName,serverContent:this.newServerContent})
+  onAddBlueprint(serverNameInput:HTMLInputElement) {
+    this.blueprintCreated.emit({serverName:serverNameInput.value,serverContent:this.newServerContent})
   }
 
 }
