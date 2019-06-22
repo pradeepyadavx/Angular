06a1432fcbddfe9f24b1a6756cb2d7b6ced36fb9
pradeep.yadav <pradeep.yadav@bdb.ai>
2019-06-22 18:40:31 +0530

    Add recipe list to other recile detasils

--- recipe-book-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,23 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  @Output() recipeWasSeleted= new EventEmitter<Recipe>();
   recipes: Recipe[]=[
-    new Recipe('Test recipe1','Desc  of test recipe','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Test recipe2','Desc  of test recipe','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe('Test recipe1','Desc  of test recipe 1','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Test recipe2','Desc  of test recipe 2','https://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2019/03/04141012/lime-roasted-salmon-skillet-square-500x500.jpg'),
+    new Recipe('Test recipe3','Desc  of test recipe 3','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Test recipe4','Desc  of test recipe 4','https://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2019/03/04141012/lime-roasted-salmon-skillet-square-500x500.jpg')
   ];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSeleted(recipe:Recipe){
+    this.recipeWasSeleted.emit(recipe);
+
+
   }
 
 }
