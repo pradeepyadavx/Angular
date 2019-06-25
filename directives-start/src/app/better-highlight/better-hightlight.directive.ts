6ddd71ff9ec112ebff59022df7e3e9b7cbd3a275
pradeep.yadav <pradeep.yadav@bdb.ai>
2019-06-25 12:37:18 +0530

    add mouse event hover using Hostlistner

--- directives-start/src/app/better-highlight/better-hightlight.directive.ts
+++ directives-start/src/app/better-highlight/better-hightlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, OnInit, Renderer2, ElementRef } from '@angular/core';
+import { Directive, OnInit, Renderer2, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHightlight]'
@@ -8,6 +8,13 @@ export class BetterHightlightDirective implements OnInit {
   constructor(private elRef:ElementRef ,private render:Renderer2) { }
 
   ngOnInit(){
+   // this.render.setStyle(this.elRef.nativeElement,'background-color','blue');
+  }
+
+  @HostListener('mouseenter') mouseover(eventData:Event){
     this.render.setStyle(this.elRef.nativeElement,'background-color','blue');
+  }
+  @HostListener('mouseleave') mouseleave(eventData:Event){
+    this.render.setStyle(this.elRef.nativeElement,'background-color','pink');
   }
 }
