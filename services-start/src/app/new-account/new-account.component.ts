c50453b86cea5dc05a5e4c6368830392a518a11c
pradeep.yadav <pradeep.yadav@bdb.ai>
2019-06-26 14:35:57 +0530

    add data using service so remove all ouputs make it centrilize

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,21 +1,22 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountService } from '../accont.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css']
+ 
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+ 
+
+  constructor(private loggingService:LoggingService,private accountService:AccountService){}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    
-    const service= new LoggingService();
-    service.logStatusChange(accountStatus);
+    this.accountService.addAccount(accountName,accountStatus);
+
+   // this.loggingService.logStatusChange(accountStatus);
+   
   }
 }
