713beab75d370d9d09490de0fb7840f03f4bf5a0
pradeep.yadav <pradeep.yadav@bdb.ai>
2019-07-07 16:36:23 +0530

    add routing the haeder using routerLink

--- recipe-book-app/src/app/header/header.component.ts
+++ recipe-book-app/src/app/header/header.component.ts
@@ -1,17 +1,8 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
     selector:'app-header',
     templateUrl:'./header.component.html'
 })
 export  class HeaderComponent{
-    
-@Output() featureSeleted=new EventEmitter<String>();
-
-    onSeleect(feature:String){
-        this.featureSeleted.emit(feature);
-
-    }
-
-
 } 
