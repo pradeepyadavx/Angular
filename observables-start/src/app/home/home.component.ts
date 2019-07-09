52a436167bad6d740ba7180512c6c17fea479296
pradeep.yadav <pradeep.yadav@bdb.ai>
2019-07-09 20:00:26 +0530

    add observables project and add basic observale for time interval

---
+++ observables-start/src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    const myNumber=Observable.interval(1000)
+    myNumber.subscribe(
+      (number:number)=>{
+        console.log(number);
+      }
+    )
+  }
+
+
+}
