d522e81a2934175490cbdf83142891719fa095f0
pradeep.yadav <pradeep.yadav@bdb.ai>
2019-07-11 19:49:54 +0530

    add custom obervabkle and destroy

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -1,24 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx'
+import { Observer, Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit,OnDestroy {
+
+  numberObssubs:Subscription;
+  customObssubs:Subscription;
 
   constructor() { }
 
   ngOnInit() {
     const myNumber=Observable.interval(1000)
-    myNumber.subscribe(
+   this.numberObssubs= myNumber.subscribe(
       (number:number)=>{
         console.log(number);
       }
     )
+
+    const myobersvable=Observable.create((observer:Observer<string>) =>{
+      setTimeout(() =>{
+        observer.next("first package")
+      },2000);
+      setTimeout(() =>{
+        observer.next("second package")
+      },4000);
+      setTimeout(() =>{
+        //observer.error("this is not working")
+        observer.complete();
+      },5000);
+      setTimeout(() =>{
+        observer.next("fourth package")
+      },6000);
+  
+    });
+    this.customObssubs=myobersvable.subscribe(
+      (data:string)=>{console.log(data)},
+      (error:string)=>{console.log(error)},
+      () =>{ console.log("complted")}
+    );
   }
 
+
+  ngOnDestroy(){
+    this.numberObssubs.unsubscribe();
+    this.customObssubs.unsubscribe();
+  }
 
 }
