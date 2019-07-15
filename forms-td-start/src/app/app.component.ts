98989f9cc96a71bfaacce24d13a1887285d53620
pradeep.yadav <pradeep.yadav@bdb.ai>
2019-07-15 19:30:41 +0530

    using view child

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,11 +7,15 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') data:ElementRef;
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
+
+
   onSubmit(form:NgForm){
     console.log(form);
+    console.log(this.data);
   }
 }
